8db338aeeb484f803c1a70015047caf907034d9e
Jordan <jsscottsama@gmail.com>
2026-01-26 11:00:23 +0100

    page auteur

--- flaskr/author.py
+++ flaskr/author.py
@@ -8,13 +8,12 @@ from flaskr.db import get_db
 bp = Blueprint('author', __name__)
 
 @bp.route('/author/<int:id>')
-@login_required
 def index(id):
     db = get_db()
     author = db.execute(
         'SELECT * FROM author WHERE author_id = ? ',(id,)
     ).fetchone()
     books = db.execute(
-        'SELECT * FROM books b WHERE b.author_id = ? ', (id)
+        'SELECT * FROM books b WHERE b.author_id = ? ',(id,)
     ).fetchall()
     return render_template('author/index.html', author=author, books=books)
